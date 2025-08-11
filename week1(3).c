b1daff33df444e02b9a7b0b33ba0ae7a2001f34e
SOURAV-7869 <semwalsourav055@gmail.com>
2025-08-11 08:29:11 +0530

    improve

--- week1(3).c
+++ week1(3).c
@@ -3,10 +3,12 @@
 int main()
 {
     int T;
+    printf("enter the how many test case you want try:")
     scanf("%d",&T);
     while(T--)
     {
         int n;
+        printf("enter the size of n :");
         scanf("%d",&n);
         int num,result = 0;
         for(int i = 0 ; i<n ; i++)
