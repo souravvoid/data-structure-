84b69f9b7087c1e1a53a577d9e86dad44516a617
https://github.com/souravvoid <125134322+souravvoid@users.noreply.github.com>
2025-08-30 11:53:48 +0530

    add week4 qued 3

--- week4/week2.c
+++ week4/week2.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-#include<c.type.h>
+#include<ctype.h>
 #include<stdlib.h>
 #define MAX 100 
 
 
 void enqueue(int queue []  ,int *front,int *rear)
-{
-    int(*rear == MAX -1)
+{   int value;
+    if(*rear == MAX -1)
     {
-        printf("queue is full")
+        printf("queue is full");
     }
-    else if (*front == && *rear == -1)
+    else if (*front == -1 && *rear == -1) 
     {
        *front ,*rear =0 ;
        queue[*rear] = value;
@@ -27,7 +27,7 @@ void dequeue(int queue[],int *front ,int *rear)
 {   ++(*front);
     if(*front == -1 && *rear == -1)
     {
-         printf("queue is empty")
+         printf("queue is empty");
     }
     else if (*front == *rear)
     {
@@ -37,14 +37,14 @@ void dequeue(int queue[],int *front ,int *rear)
     }
      
     else{
-        printf("dequeue element = %d",queue[front]);
+        printf("dequeue element = %d",queue[*front]);
     }
     
 
     
 }
 
-int dequeue(int queue[],int *rear,int *front)
+int deque(int queue[],int *rear,int *front)
 {
     if(*front == -1 && *rear == -1)
      {
@@ -58,10 +58,13 @@ void display(int queue[] ,int *front,int *rear )
     {
         printf("queue is empty");
     }
+    else
+    {
   for(int i = *front;i<=*rear;i++)
   {
     printf("%d",queue[i]);
   }
+   }
 }
 
 
@@ -71,8 +74,100 @@ int main()
     int front = -1;
     int rear = -1;
 
-    enqueue();
-    dequeue();
-    display();
+    enqueue(queue,&front ,&rear,10,20);
+    enqu
+    dequeue( queue,&front,&rear ,10);
+    deque(queue,&front,&rear);
+    display(queue,&front ,&rear);
     return 0;
 }
+
+/*
+#include <stdio.h>
+
+    void enqueue(int queue[], int *front, int *rear, int n)
+{
+
+    if (*rear == 99)
+    {
+        printf("Queue is full");
+        return;
+    }
+    else if (*rear == -1 && *front == -1)
+    {
+
+        *front = 0;
+        *rear = 0;
+        queue[*rear] = n;
+    }
+    else
+    {
+        ++*rear;
+        queue[*rear] = n;
+    }
+}
+
+void dequeue(int queue[], int *front, int *rear)
+{
+
+    if (*front == -1 && *rear == -1)
+    {
+
+        printf("Queue is empty");
+    }
+    else if (*front == *rear)
+    {
+
+        *front = -1;
+        *rear = -1;
+    }
+    printf("Element deleted= %d\n", queue[*front]);
+    ++(*front);
+}
+
+int Front(int queue[], int *front, int *rear)
+{
+
+    if (*front == -1 && *rear == -1)
+    {
+
+        printf("Queue is empty");
+        return -1;
+    }
+    return queue[*front];
+}
+
+void display(int queue[], int *front, int *rear)
+{
+
+    if (*front == -1 && *rear == -1)
+    {
+
+        printf("Queue is empty");
+    }
+    printf("%d %d\n", *front, *rear);
+    for (int i = *front; i <= *rear; i++)
+    {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main()
+{
+
+    int front = -1;
+    int rear = -1;
+    int queue[100];
+    enqueue(queue, &front, &rear, 10);
+    enqueue(queue, &front, &rear, 20);
+    enqueue(queue, &front, &rear, 30);
+    enqueue(queue, &front, &rear, 40);
+    display(queue, &front, &rear);
+    printf("\n");
+    dequeue(queue, &front, &rear);
+    printf("%d", Front(queue, &front, &rear));
+    printf("\n");
+    display(queue, &front, &rear);
+}
+
+*/
